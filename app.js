08abbf9682cacb79b847f22648f40940ce4f8fe5
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-15 18:37:13 +0200

    Add morgan as a logger middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var logger = require('morgan');
 
 app.listen(3000);
 
+app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
