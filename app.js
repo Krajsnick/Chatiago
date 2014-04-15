8926eaed342207c40a89b6783475752a8bd4bcca
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-15 19:15:45 +0200

    Mount socket.io to server (and client) instance and restructure code

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
-var logger = require('morgan');
-
-app.listen(3000);
+var express = require('express'),
+    app = express(),
+    server = app.listen(3000),
+    io = require('socket.io').listen(server),
+    logger = require('morgan');
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
