8fb51cdeac30fec19b517db85f4157121c0c6163
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-15 23:49:12 +0200

    Basic chat functionality

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ var express = require('express'),
 
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
+
+
+io.sockets.on('connection', function(socket) {
+
+  socket.on('msg-received', function(msgData) {
+    console.log('Message received from client:\n' + msgData.name+': ' + msgData.message);
+    io.sockets.emit('update-chat', msgData);
+  });
+
+});
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,17 @@
 var ws = io.connect(window.location.origin);
+
+$('#chat-message').keypress(function(e) {
+  if (e.keyCode == 13) {
+    console.log("Chat message sent");
+    var message = $(this).val();
+
+    ws.emit('msg-received', {name: 'Robert', message: message});
+
+    $(this).val('');
+  }
+});
+
+ws.on('update-chat', function(msgData) {
+  var htmlMsg = "<b>" + msgData.name + ":</b> " + msgData.message + "<br />";
+  $('#chat').append(htmlMsg);
+});
