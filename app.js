a9d713330b33eecaa011fa680d452d235898aa1e
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-17 01:09:15 +0200

    Maintain list of connected names and prepend message with current name

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ var express = require('express'),
 app.use(logger('dev'));
 app.use(express.static(__dirname + '/public'));
 
+var connectedNames = [];
 
 io.sockets.on('connection', function(socket) {
 
   socket.on('msg-received', function(msgData) {
     console.log('Message received from client:\n' + msgData.name+': ' + msgData.message);
     io.sockets.emit('update-chat', msgData);
+  });
+
+  socket.on('set-name', function(data) {
+    socket.set('name', data, function() {
+      connectedNames.push(data);
+      console.log("Entered name: " + data);
+      io.sockets.emit('new-user-connected', connectedNames);
+    });
+  });
+
+  socket.on('disconnect', function() {
+    socket.get('name', function(err, name) {
+      var index = connectedNames.indexOf(name);
+      connectedNames.splice(index, 1);
+      io.sockets.emit('new-user-connected', connectedNames);
+    });
   });
 
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,24 @@
 var ws = io.connect(window.location.origin),
     $chat = $('#chat');
 
+$('#chat-name').focus();
+
+if ($('#chat-name').keypress(function(e) {
+  if (($('#chat-name').val() != "") && e.keyCode == 13) {
+    nickname = $('#chat-name').val();
+    $('#chat-name-row').fadeOut('fast', function() {
+      $('#chat-row, #chat-message').fadeIn('slow');
+    });
+    ws.emit('set-name', nickname);
+  }
+}));
+
 $('#chat-message').keypress(function(e) {
   if (e.keyCode == 13) {
     console.log("Chat message sent");
     var message = $(this).val();
 
-    ws.emit('msg-received', {name: 'Robert', message: message});
+    ws.emit('msg-received', {name: nickname, message: message});
 
     $(this).val('');
   }
@@ -15,4 +27,14 @@ $('#chat-message').keypress(function(e) {
 ws.on('update-chat', function(msgData) {
   var htmlMsg = "<b>" + msgData.name + ":</b> " + msgData.message + "<br />";
   $chat.append(htmlMsg).scrollTop($chat[0].scrollHeight);
+});
+
+ws.on('new-user-connected', function(data) {
+  var userLiHtml = "";
+
+  data.forEach(function(name) {
+    userLiHtml += "<li>" + name + "</li>";
+  });
+
+  $('#user-list ul').html(userLiHtml);
 });
